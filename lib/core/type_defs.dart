9e012d23faab223a2e422858a70966bd461e4290
Kiishi David <thatsaxydev@gmail.com>
2024-08-04 21:28:38 +0100

    refactored fpdart typedefs

--- lib/core/type_defs.dart
+++ lib/core/type_defs.dart
@@ -1,5 +1,15 @@
 import 'package:fpdart/fpdart.dart';
 import 'package:quizlee/core/failure.dart';
 
-typedef FutureEither<T> = Future<Either<Failure, T>>;
-typedef FutureVoid = FutureEither<void>; 
+//! FUTURE EITHER, IS A CUSTOM TYPE DEF.
+//! IT EQUATES A FUTURE OF TYPE EITHER, WHERE EITHER IS A CUSTOM TYPE DEF THAT RETURNS FAILURE RESPONSE ON THE LEFT AND SUCCESS ON THE RIGHT.
+//! FAILURE TYPE HAS BEEN CUSTOM DEFINED TO RETURN JUST THE FAILURE MESSAGE.
+//! SUCCESS TYPE IS DEPENDENT ON DEVELOPER DEFINITION. IT CAN BE AN INT, A BOOL OR WHATEVER.
+typedef FutureEither<SuccessType> = Future<Either<Failure, SuccessType>>;
+
+//! SIMPLE FUTURE OF TYPE VOID RETURNING A CUSTOM FUTURE EITHER OF TYPE VOID I.E
+//! REGULAR FUTURE THAT RETURNS NOTHING.
+typedef FutureVoid = FutureEither<void>;
+
+//! ENUM FOR NOTIFICATION TYPE
+enum NotificationType { success, failure, info }
