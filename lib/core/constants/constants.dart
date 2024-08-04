93166e7e9b5a139e110506036af63288f79bf268
Kiishi David <thatsaxydev@gmail.com>
2024-08-04 21:34:20 +0100

    refactored constants file

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,6 +1,4 @@
 class Constants {
-  static const bannerDefault =
-      'https://umis.babcock.edu.ng/babcock/assets/landing/images/bu_logo_main_white_bg.jpg';
   static const avatarDefault =
       'https://freesvg.org/img/abstract-user-flat-4.png';
 }
