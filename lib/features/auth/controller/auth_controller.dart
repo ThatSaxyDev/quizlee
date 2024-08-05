11689f740e43dea0c73a59685c2c3b4fef425c61
Kiishi David <thatsaxydev@gmail.com>
2024-08-05 07:55:51 +0100

    refactored auth controller to use show banner

--- lib/features/auth/controller/auth_controller.dart
+++ lib/features/auth/controller/auth_controller.dart
@@ -2,8 +2,10 @@ import 'dart:developer';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:quizlee/core/type_defs.dart';
 import 'package:quizlee/features/auth/repositories/auth_repository.dart';
 import 'package:quizlee/models/user_model.dart';
+import 'package:quizlee/utils/banner.dart';
 import 'package:quizlee/utils/snack_bar.dart';
 
 final userProvider = StateProvider<UserModel?>((ref) => null);
@@ -42,7 +44,11 @@ class AuthController extends StateNotifier<bool> {
     user.fold(
       (l) {
         log(l.message);
-        showSnackBar(context, l.message);
+        showBanner(
+          context: context,
+          theMessage: l.message,
+          theType: NotificationType.failure,
+        );
       },
       (userModel) =>
           _ref.read(userProvider.notifier).update((state) => userModel),
